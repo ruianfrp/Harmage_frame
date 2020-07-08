f2e9c0a5b13a4cecc60b2241c8c1c318c203c6f9
Ray <frp@harmonycloud.cn>
2020-07-08 15:23:29 +0800

    2020.7.8  目录结构修改

--- src/main/java/com/harmonycloud/imp/ProjectMemberSkillServiceImp.java
+++
@@ -1,9 +0,0 @@
-package com.harmonycloud.imp;
-
-import com.harmonycloud.service.ProjectMemberSkillService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class ProjectMemberSkillServiceImp implements ProjectMemberSkillService {
-
-}
